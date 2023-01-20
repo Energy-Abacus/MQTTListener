69e82fccc54db3e8d5deeb95094d14f74bf03d62
Sedad Sultanovic <sultansedad@gmail.com>
2023-01-20 08:17:38 +0100

    final version

--- src/main/java/org/abacus/script/App.java
+++ src/main/java/org/abacus/script/App.java
@@ -1,10 +1,15 @@
 package org.abacus.script;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.paho.client.mqttv3.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.*;
 
 public class App {
 
@@ -18,11 +23,12 @@ public class App {
         String deviceId;
         JSONtoJava classObject = new JSONtoJava();
         String topic = classObject.getIdAsString();
-        List<String> subs = classObject.getSubscriptions();
-        List<String> subsWithId = new ArrayList<>();
-        boolean subscribed = false;
+        Map<String,String> subs = classObject.getSubscriptions();
+        Map<String,String> data = new HashMap<>();
+        Map<String,String> temp = new HashMap<>();
         int qos = 1;
         IMqttClient client;
+        /*https://student.cloud.htl-leonding.ac.at/e.gstallnig/abacus-backend/api/v1/Measurements*/
 
 
         try {
@@ -37,23 +43,43 @@ public class App {
                     System.out.println("connectionLost: " + cause.getMessage());
                 }
 
-                public void messageArrived(String topic, MqttMessage message) throws MqttException {
+                public void messageArrived(String topic, MqttMessage message) throws MqttException, IOException, InterruptedException {
                     System.out.println("topic: " + topic);
                     System.out.println("Qos: " + message.getQos());
                     System.out.println("message content: " + new String(message.getPayload()));
 
-                    String deviceId = "";
+                    String deviceId;
 
                     if(new String(message.getPayload()).startsWith("{")) {
                         deviceId = classObject.getDeviceId(new String(message.getPayload()));
-                        System.out.println(deviceId);
-
-                        for (String item: subs) {
-                            System.out.println(item);
-                            subsWithId.add(item.replace("{id}",deviceId));
-                            subWithId(item.replace("{id}",deviceId), client);
+                        classObject.deviceId = deviceId;
+                        for (Map.Entry<String,String> item : subs.entrySet()) {
+                            subWithId(item.getValue().replace("{id}",deviceId), client);
                         }
                     }
+                    else{
+                        String strTopic = "";
+                        for (Map.Entry<String,String> item: subs.entrySet()) {
+                            if(item.getValue().replace("{id}",classObject.deviceId).equals(topic)){
+                                strTopic = item.getKey();
+                            }
+                        }
+
+                        data.put(strTopic,new String(message.getPayload()));
+
+                        for (Map.Entry<String,String> item: data.entrySet()) {
+                            if(data.containsKey(item.getKey())){
+                                temp.put(item.getKey(),item.getValue());
+                            }
+                        }
+
+                        if(temp.size() == 4) {
+                            postToServer(temp);
+                            data.clear();
+                            temp.clear();
+                        }
+                    }
+
                 }
 
                 public void deliveryComplete(IMqttDeliveryToken token) {
@@ -74,5 +100,23 @@ public class App {
 
     static void subWithId(String topic, IMqttClient client) throws MqttException {
         client.subscribe(topic,1);
+    }
+
+    static void postToServer(Map<String,String> data) throws IOException, InterruptedException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(data);
+
+        System.out.println(requestBody);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://student.cloud.htl-leonding.ac.at/e.gstallnig/abacus-backend/api/v1/Measurements"))
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
     }
 }
--- src/main/java/org/abacus/script/JSONtoJava.java
+++ src/main/java/org/abacus/script/JSONtoJava.java
@@ -11,9 +11,11 @@ import org.json.simple.parser.ParseException;
 public class JSONtoJava {
 
     String id;
-    List<String> subs = new ArrayList<>();
+    Map<String,String> subs = new HashMap<>();
+    public String deviceId;
 
     {
+
         JSONParser parser = new JSONParser();
         try {
             Object object;
@@ -25,10 +27,10 @@ public class JSONtoJava {
             id = (String)idObject.get("id");
             JSONObject interfaces = (JSONObject)idObject.get("interfaces");
 
-            subs.add((String)interfaces.get("status"));
-            subs.add((String)interfaces.get("wattPower"));
-            subs.add((String)interfaces.get("wattMinutePower"));
-            subs.add((String)interfaces.get("temperature"));
+            subs.put("status",(String)interfaces.get("status"));
+            subs.put("wattPower",(String)interfaces.get("wattPower"));
+            subs.put("wattMinutePower",(String)interfaces.get("wattMinutePower"));
+            subs.put("temperature",(String)interfaces.get("temperature"));
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
@@ -38,7 +40,7 @@ public class JSONtoJava {
         return id;
     }
 
-    public List<String> getSubscriptions(){
+    public Map<String,String> getSubscriptions(){
         return subs;
     }
 
